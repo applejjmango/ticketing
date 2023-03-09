4b3d096feeb1604c04177488748ae32920ef412b
play-builder <playbuilder47@gmail.com>
2023-03-09 11:15:48 +0900

    udpated

---
+++ auth/src/index.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDb....");
+  } catch (err) {
+    console.error(err);
+  }
+  console.log(app.listen);
+  console.log("app => ", app);
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!");
+  });
+};
+
+start();
